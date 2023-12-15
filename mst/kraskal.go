38977b44f908c49fc9f42f8f43ab9e772901a2c1
Vitaliy <sw.vitaliy@gmail.com>
2023-12-16 00:55:38 +0400

    mst

--- mst/kraskal.go
+++ mst/kraskal.go
@@ -1,1 +1,30 @@
 package mst
+
+import (
+	"goalgo/lca"
+	"sort"
+)
+
+type edge struct {
+	from, to int
+	w        wei
+}
+
+// KraskalMST for minimum spanning tree
+func KraskalMST(g []edge, n int) []edge {
+	m := len(g)
+	var res []edge
+	sort.Slice(g, func(i, j int) bool {
+		return g[i].w < g[j].w
+	})
+
+	d := lca.NewDSU(n)
+	for i := 0; i < m; i++ {
+		if d.FindSet(g[i].from) != d.FindSet(g[i].to) {
+			res = append(res, g[i])
+			d.UnionSets(g[i].from, g[i].to, g[i].from)
+		}
+	}
+
+	return res
+}
--- mst/prim.go
+++ mst/prim.go
@@ -1,11 +1,54 @@
 package mst
 
+import (
+	"goalgo/limits"
+	"goalgo/pq"
+	"goalgo/slices"
+	"math"
+)
+
 type ndx int
 type wei int
 
-func Prim(s, t int, g []struct {
+func init() {
+	limits.AddTypeLimits[ndx](limits.Limits{
+		MaxValue: math.MaxInt,
+		MinValue: math.MinInt,
+	})
+	limits.AddTypeLimits[wei](limits.Limits{
+		MaxValue: math.MaxInt,
+		MinValue: math.MinInt,
+	})
+}
+
+// adjEdge for adjacent matrix graph representation
+type adjEdge struct {
 	t ndx
 	w wei
-}) {
+}
 
+// PrimMST for minimum spanning tree
+func PrimMST(s ndx, g [][]adjEdge) []ndx {
+	n := len(g)
+	d := make([]wei, n)
+	slices.Fill(d, limits.MaxValue[wei]())
+	d[s] = 0
+	p := make([]ndx, n)
+	p[s] = -1
+
+	q := pq.NewPriorQueue[ndx, wei]()
+	q.Enqueue(0, s)
+	for len(q) > 0 {
+		v, _ := q.Dequeue()
+		for _, e := range g[v] {
+			if e.w < d[e.t] {
+				// TODO replace with btree
+				d[e.t] = e.w
+				p[e.t] = v
+				q.Enqueue(d[e.t], e.t)
+			}
+		}
+	}
+
+	return p
 }
