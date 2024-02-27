66fe4a3718b03e4169cf96b8ccba4f5b620af4fb
Vitaliy <sw.vitaliy@gmail.com>
2024-02-28 01:42:15 +0400

    binSearch, lowerBound, upperBound

--- slices/binsearch.go
+++ slices/binsearch.go
@@ -17,3 +17,74 @@ func BinSearch[S ~[]T, T cmp.Ordered](a S, v T) int {
 	}
 	return -1
 }
+
+func BinSearchN(n int, cmp func(int) int) int {
+	i, j := 0, n
+	for i <= j {
+		m := (i + j) / 2
+		compareResult := cmp(m)
+		if compareResult == 0 {
+			return m
+		}
+		if compareResult > 0 {
+			i = m + 1
+		} else {
+			j = m - 1
+		}
+	}
+	return -1
+}
+
+func UpperBound[S ~[]T, T cmp.Ordered](arr S, v T) int {
+	l, r, m := 0, len(arr), 0
+
+	for l < r {
+		m = (l + r) / 2
+		if cmp.Less(v, arr[m]) {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
+
+func UpperBoundN(n int, cmp func(int) int) int {
+	l, r, m := 0, n, 0
+	for l < r {
+		m = (l + r) / 2
+		if cmp(m) == 1 { // less than
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
+
+func LowerBound[S ~[]T, T cmp.Ordered](arr S, v T) int {
+	l, r, m := 0, len(arr), 0
+
+	for l < r {
+		m = (l + r) / 2
+		if cmp.Less(arr[m], v) {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+	return l
+}
+
+func LowerBoundN(n int, cmp func(int) int) int {
+	l, r, m := 0, n, 0
+	for l < r {
+		m = (l + r) / 2
+		if cmp(m) == -1 {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+	return l
+}
--- slices/binsearch_test.go
+++ slices/binsearch_test.go
@@ -10,14 +10,44 @@ func TestBinSearch(t *testing.T) {
 	assert.Equal(t, 2, BinSearch(a, 2), "BinSearch failed")
 }
 
-func TestBinSearch2(t *testing.T) {
+func TestBinSearchN(t *testing.T) {
 	a := []int{1, 3, 2, 7, 0}
+	cmp := func(v int) func(int) int {
+		return func(i int) int {
+			if a[i] == v {
+				return 0
+			} else if a[i] < v {
+				return -1
+			}
+			return 1
+		}
+	}
+	assert.Equal(t, 2, BinSearchN(len(a), cmp(2)), "BinSearch failed")
+}
+
+func TestBinSearch2(t *testing.T) {
+	a := []int{0, 1, 2, 3, 7}
 	assert.Equal(t, -1, BinSearch(a, 5), "BinSearch failed")
 }
 
+func TestBinSearchN2(t *testing.T) {
+	a := []int{0, 1, 2, 3, 7}
+	cmp := func(v int) func(int) int {
+		return func(i int) int {
+			if a[i] == v {
+				return 0
+			} else if a[i] < v {
+				return -1
+			}
+			return 1
+		}
+	}
+	assert.Equal(t, -1, BinSearchN(len(a), cmp(5)), "BinSearch failed")
+}
+
 func TestBinSearch3(t *testing.T) {
-	a := []int{1, 3, 2, 7, 0}
-	assert.Equal(t, 0, BinSearch(a, 1), "BinSearch failed")
+	a := []int{0, 1, 2, 3, 7}
+	assert.Equal(t, 1, BinSearch(a, 1), "BinSearch failed")
 }
 
 func TestBinSearch4(t *testing.T) {
@@ -28,4 +58,50 @@ func TestBinSearch4(t *testing.T) {
 func TestBinSearch5(t *testing.T) {
 	a := []int{1, 3, 2, 7, 0}
 	assert.Equal(t, -1, BinSearch(a, -10), "BinSearch failed")
+}
+
+func TestUpperLower1(t *testing.T) {
+	a := []int{1, 7, 23, 56, 67}
+	assert.Equal(t, 2, LowerBound(a, 23), "LowerBound failed")
+	assert.Equal(t, 3, UpperBound(a, 23), "UpperBound failed")
+}
+
+func TestUpperLower2(t *testing.T) {
+	a := []int{1, 7, 23, 56, 67}
+	assert.Equal(t, 2, LowerBound(a, 17), "LowerBound failed")
+	assert.Equal(t, 2, UpperBound(a, 17), "UpperBound failed")
+}
+
+func TestUpperLowerN1(t *testing.T) {
+	a := []int{1, 7, 23, 56, 67}
+	cmp := func(v int) func(int) int {
+		return func(i int) int {
+			if a[i] == v {
+				return 0
+			}
+			if a[i] < v {
+				return -1
+			}
+			return 1
+		}
+	}
+	assert.Equal(t, 2, LowerBoundN(len(a), cmp(23)), "LowerBound failed")
+	assert.Equal(t, 3, UpperBoundN(len(a), cmp(23)), "UpperBound failed")
+}
+
+func TestUpperLowerN2(t *testing.T) {
+	a := []int{1, 7, 23, 56, 67}
+	cmp := func(v int) func(int) int {
+		return func(i int) int {
+			if a[i] == v {
+				return 0
+			}
+			if a[i] < v {
+				return -1
+			}
+			return 1
+		}
+	}
+	assert.Equal(t, 2, UpperBoundN(len(a), cmp(17)), "UpperBound failed")
+	assert.Equal(t, 2, LowerBoundN(len(a), cmp(17)), "LowerBound failed")
 }
