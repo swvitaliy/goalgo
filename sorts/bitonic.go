4edaf34b91f9a603162e5d5d4ba0193f9b640627
Vitaliy <sw.vitaliy@gmail.com>
2024-04-14 22:00:36 +0400

    bitonic sort for any length of slice

--- sorts/bitonic.go
+++ sorts/bitonic.go
@@ -1,16 +1,44 @@
 package sorts
 
-import "cmp"
+import (
+	"cmp"
+	"goalgo/limits"
+	"goalgo/slices"
+	"math/bits"
+)
 
-func BitonicSort[T cmp.Ordered](a []T, dir bool) {
+const Ascending = true
+const Descending = false
+
+func BitonicSort[T cmp.Ordered](a []T, dir bool) []T {
+	m := len(a)
+	a = bitonicFillUntil2k(a, dir)
 	n := len(a)
-	if n == 1 {
-		return
+	if n <= 1 {
+		return a
 	}
 	k := n / 2
-	BitonicSort(a[:k], dir)
-	BitonicSort(a[k:], !dir)
+	BitonicSort(a[:k], Ascending)
+	BitonicSort(a[k:], Descending)
 	bitonicMerge(a, dir)
+	return a[:m]
+}
+
+func bitonicFillUntil2k[T cmp.Ordered](a []T, dir bool) []T {
+	n := len(a)
+	k := 63 - bits.LeadingZeros64(uint64(n)) // lg(n)
+	m := 1 << k
+	if n == m {
+		return a
+	}
+	m = 1 << (k + 1)
+	a = append(a, make([]T, m-n)...)
+	if dir == Ascending {
+		slices.Fill(a[n:], limits.MaxValue[T]())
+	} else {
+		slices.Fill(a[n:], limits.MinValue[T]())
+	}
+	return a
 }
 
 func bitonicMerge[T cmp.Ordered](a []T, dir bool) {
--- sorts/bitonic_test.go
+++ sorts/bitonic_test.go
@@ -1,18 +1,30 @@
 package sorts
 
 import (
+	"github.com/stretchr/testify/assert"
 	"goalgo/slices"
 	"testing"
 )
 
 func TestBitonicSort(t *testing.T) {
 	a := []int{1, 4, 6, 6, 32, 2, 7, 3, 34, 6, 6}
-	BitonicSort(a, true)
-	if !slices.IsSorted(a, true) {
-		t.Fail()
-	}
-	BitonicSort(a, false)
-	if !slices.IsSorted(a, false) {
-		t.Fail()
-	}
+	aSorted := []int{1, 2, 3, 4, 6, 6, 6, 6, 7, 32, 34}
+	a = BitonicSort(a, Ascending)
+	assert.True(t, slices.IsSorted(a, slices.Ascending))
+	assert.Equal(t, aSorted, a)
+
+}
+func TestBitonicSortDescending(t *testing.T) {
+	a := []int{1, 4, 6, 6, 32, 2, 7, 3, 34, 6, 6}
+	aSorted := []int{1, 2, 3, 4, 6, 6, 6, 6, 7, 32, 34}
+	slices.Reverse(aSorted)
+	a = BitonicSort(a, Descending)
+	assert.True(t, slices.IsSorted(a, slices.Descending))
+	assert.Equal(t, aSorted, a)
+}
+
+func TestBitonicInplaceWhen2K(t *testing.T) {
+	a := []int{1, 4, 6, 6, 32, 2, 7, 3}
+	b := BitonicSort(a, Ascending)
+	assert.Equal(t, &b, &a)
 }
